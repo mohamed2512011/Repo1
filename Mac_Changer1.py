2e75744b63e15f1c2e92af692df0a5dfe2cb1afd
Mohamed Ahmed <162486515+mohamed2512011@users.noreply.github.com>
2024-12-28 19:25:26 +0200

    Update Mac_Changer1.py

--- Mac_Changer1.py
+++ Mac_Changer1.py
@@ -4,8 +4,8 @@ print("Welcome to the Mac Changer!")
 
 import subprocess
 subprocess.call("ifconfig", shell=True)
-interface = input("Enter your Interface? \n")
-mac = input("Please enter your MAC changer? \n")
+interface = input("Enter your Interface:\n")
+mac = input("Please enter your New Mac Adress:\n")
 subprocess.call(f"ifconfig {interface} down", shell=True)
 subprocess.call(f"ifconfig {interface} hw ether {mac} ", shell=True)
 subprocess.call(f"ifconfig {interface} up ", shell=True)
